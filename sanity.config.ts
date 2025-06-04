88c4737f676b12c44a3132ffeead89db803ce729
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-06-03 20:29:18 -0600

    Home Page

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {deskStructure} from './deskStructure'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: '4qydhzw9',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [visionTool(), deskTool({structure: deskStructure})],
 
   schema: {
     types: schemaTypes,
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -3,5 +3,6 @@ import post from './post'
 import event from './event'
 import tag from './tag'
 import galleryImage from './galleryImage'
+import homePage from './homePage'
 
-export const schemaTypes = [post, event, tag, galleryImage, blockContent]
+export const schemaTypes = [post, event, tag, galleryImage, blockContent, homePage]
