e6a4588438cebe378cacf330119eb88847c05c45
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-06-21 11:07:39 -0600

    Import partner in schema types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -4,5 +4,6 @@ import event from './event'
 import tag from './tag'
 import galleryImage from './galleryImage'
 import homePage from './homePage'
+import partner from './partner'
 
-export const schemaTypes = [post, event, tag, galleryImage, blockContent, homePage]
+export const schemaTypes = [post, event, tag, galleryImage, blockContent, homePage, partner]
